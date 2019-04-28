0ba8343ac220d533e74958d5ec60542b08d58858
Bartłomiej Kaczor <bkaczor@d17-l427-pc-17.ipa.iisg.agh.edu.pl>
2019-04-28 15:37:28 +0200

    travisAndPom

--- src/main/java/pl/edu/agh/mwo/calculator/Calculator.java
+++ src/main/java/pl/edu/agh/mwo/calculator/Calculator.java
@@ -7,27 +7,27 @@ package pl.edu.agh.mwo.calculator;
 public class Calculator
 {
     public int sum(int a, int b) {
-        System.out.println("Wynik dodawania: ");
+        //System.out.println("Wynik dodawania: ");
         return a+b;
     }
 
     public int sub(int a, int b) {
-        System.out.println("Wynik odejmowania: ");
+        //System.out.println("Wynik odejmowania: ");
         return a-b;
     }
 
     public int mult(int a, int b) {
-        System.out.println("Wynik mnozenia: ");
+        //System.out.println("Wynik mnozenia: ");
         return a*b;
     }
 
     public int div(int a, int b) {
-        System.out.println("Wynik dzielenia: ");
+        //System.out.println("Wynik dzielenia: ");
         return a/b;
     }
 
     public int avg(int a, int b) {
-        System.out.println("Srednia: ");
+        //System.out.println("Srednia: ");
         return (a+b)/2;
     }
 }
